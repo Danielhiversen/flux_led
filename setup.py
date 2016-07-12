16f0c7be85876dc7bf628b6d93168a0b40e28aef
Daniel Høyer Iversen <mail@dahoiv.net>
2016-07-12 13:56:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+setup(
+    name = 'pyRFXtrx',
+    packages = ['RFXtrx'],
+    install_requires=['pyserial>=2.7'],
+    version = '0.9.0',
+    description = 'a library to communicate with the RFXtrx family of devices',
+    author='Edwin Woudt',
+    author_email='edwin@woudt.nl',
+    url='setup(
+    name = 'flux_led',
+    packages = ['flux_led'],
+    version = '0.1.0',
+    url='https://github.com/Danielhiversen/pyRFXtrx',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ' +
+            'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ' +
+            'GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Home Automation',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
