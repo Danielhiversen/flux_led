f9716950a0a57fa38e1a21ce19cf0c3a132f2ac8
Johan Bloemberg <github@ijohan.nl>
2017-02-08 21:38:19 +0100

    Export utils.

--- flux_led/__init__.py
+++ flux_led/__init__.py
@@ -1,4 +1,4 @@
 """Init file for Flux LED"""
-from .__main__ import PresetPattern, LedTimer, WifiLedBulb, BulbScanner
+from .__main__ import PresetPattern, LedTimer, WifiLedBulb, BulbScanner, utils
 
-__all__ = ['PresetPattern', 'LedTimer', 'WifiLedBulb', 'BulbScanner']
+__all__ = ['PresetPattern', 'LedTimer', 'WifiLedBulb', 'BulbScanner', 'utils']
