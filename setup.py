623d301c7532fcd0cb61a6ced68ece9ed2ca07a5
Daniel Høyer Iversen <mail@dahoiv.net>
2017-09-22 07:41:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name = 'flux_led',
     packages = ['flux_led'],
-    version = '0.19',
+    version = '0.20',
     description = 'A Python library to communicate with the flux_led smart bulbs',
     author = 'Daniel Høyer Iversen, Beville',
     author_email = 'mail@dahoiv.net',
