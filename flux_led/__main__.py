37fa640b0f4c86188d03c5ad6a2ff097444bbbcf
J. Nick Koston <nick@koston.org>
2021-10-03 08:01:34 -1000

    Improve reliablity of scanner
    - Send out discovery packets 3x in case packets
      get lost.
    
    - scan now accepts an address to allow for probing
      a specific ip.

--- flux_led/__main__.py
+++ flux_led/__main__.py
@@ -40,6 +40,7 @@ package is installed.  (Easily done via pip, easy_install, or apt-get, etc.)
 from __future__ import print_function
 from enum import Enum
 import logging
+import select
 import socket
 import time
 import sys
@@ -1289,6 +1290,12 @@ class WifiLedBulb:
 
 
 class BulbScanner:
+
+    DISCOVERY_PORT = 48899
+    BROADCAST_FREQUENCY = 3
+    RESPONSE_SIZE = 64
+    BROADCAST_ADDRESS = "<broadcast>"
+
     def __init__(self):
         self.found_bulbs = []
 
@@ -1301,52 +1308,77 @@ class BulbScanner:
     def getBulbInfo(self):
         return self.found_bulbs
 
-    def scan(self, timeout=10):
+    def scan(self, timeout=10, address=None):
+        """Scan for bulbs.
 
-        DISCOVERY_PORT = 48899
-
+        If an address is provided, the scan will return
+        as soon as it gets a response from that address
+        """
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(("", DISCOVERY_PORT))
+        sock.bind(("", self.DISCOVERY_PORT))
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setblocking(0)
+        if address is None:
+            address = self.BROADCAST_ADDRESS
+        destination = (address, self.DISCOVERY_PORT)
 
         msg = "HF-A11ASSISTHREAD".encode("ascii")
 
         # set the time at which we will quit the search
-        quit_time = time.time() + timeout
-
+        quit_time = time.monotonic() + timeout
+        seen = set()
         response_list = []
+        found_all = False
         # outer loop for query send
-        while True:
-            if time.time() > quit_time:
+        while not found_all:
+            if time.monotonic() > quit_time:
                 break
             # send out a broadcast query
-            sock.sendto(msg, ("<broadcast>", DISCOVERY_PORT))
+            sock.sendto(msg, destination)
+            _LOGGER.debug("discover: %s => %s", destination, msg)
 
             # inner loop waiting for responses
             while True:
-
                 sock.settimeout(1)
-                try:
-                    data, addr = sock.recvfrom(64)
-                except socket.timeout:
-                    data = None
-                    if time.time() > quit_time:
-                        break
+                remain_time = quit_time - time.monotonic()
+                time_out = min(remain_time, timeout / self.BROADCAST_FREQUENCY)
+                if time_out <= 0:
+                    break
+                read_ready, _, _ = select.select([sock], [], [], time_out)
+                if not read_ready:
+                    if time.monotonic() < quit_time:
+                        # No response, send broadcast again in cast it got lost
+                        _LOGGER.debug("discover: %s => %s", destination, msg)
+                        sock.sendto(msg, destination)
+                    continue
 
+                try:
+                    data, addr = sock.recvfrom(self.RESPONSE_SIZE)
+                    _LOGGER.debug("discover: %s <= %s", addr, data)
+                except socket.timeout:
+                    continue
                 if data is None:
                     continue
                 if data == msg:
                     continue
 
-                data = data.decode("ascii")
-                data_split = data.split(",")
+                data_split = data.decode("ascii").split(",")
                 if len(data_split) < 3:
                     continue
-                item = dict()
-                item["ipaddr"] = data_split[0]
-                item["id"] = data_split[1]
-                item["model"] = data_split[2]
-                response_list.append(item)
+                if data_split[0] in seen:
+                    continue
+                seen.add(data_split[0])
+                response_list.append(
+                    {
+                        "ipaddr": data_split[0],
+                        "id": data_split[1],
+                        "model": data_split[2],
+                    }
+                )
+                if address == data_split[0]:
+                    found_all = True
+                    break
 
         self.found_bulbs = response_list
         return response_list
