d4c0f5f11012fd3b930d2d319223ec5455771be1
J. Nick Koston <nick@koston.org>
2021-10-08 08:33:00 -1000

    tweaks

--- flux_led/utils.py
+++ flux_led/utils.py
@@ -182,13 +182,59 @@ def rgbww_brightness(
     if not brightness or brightness == current_brightness:
         return rgbww_data
 
-    if brightness < current_brightness:
-        change_brightness_pct = (current_brightness - brightness) / current_brightness
+    color_brightness, cw_brightness, ww_brightness = _adjust_brightness(
+        current_brightness, brightness, color_brightness, cw_brightness, ww_brightness
+    )
+    r, g, b = colorsys.hsv_to_rgb(h, s, color_brightness / 255)
+    return (
+        round(r * 255),
+        round(g * 255),
+        round(b * 255),
+        ww_brightness,
+        cw_brightness,
+    )
+
+
+def rgbcw_brightness(
+    rgbcw_data: Tuple[int, int, int, int, int],
+    brightness: Optional[int] = None,
+) -> Tuple[int, int, int, int, int]:
+    """Convert rgbww to brightness."""
+    original_r, original_g, original_b = rgbcw_data[0:3]
+    h, s, v = colorsys.rgb_to_hsv(original_r / 255, original_g / 255, original_b / 255)
+    color_brightness = round(v * 255)
+    cw_brightness = rgbcw_data[3]
+    ww_brightness = rgbcw_data[4]
+    current_brightness = round((color_brightness + ww_brightness + cw_brightness) / 3)
+
+    if not brightness or brightness == current_brightness:
+        return rgbcw_data
+
+    color_brightness, cw_brightness, ww_brightness = _adjust_brightness(
+        current_brightness, brightness, color_brightness, cw_brightness, ww_brightness
+    )
+    r, g, b = colorsys.hsv_to_rgb(h, s, color_brightness / 255)
+    return (
+        round(r * 255),
+        round(g * 255),
+        round(b * 255),
+        cw_brightness,
+        ww_brightness,
+    )
+
+
+def _adjust_brightness(
+    current_brightness, new_brightness, color_brightness, cw_brightness, ww_brightness
+):
+    if new_brightness < current_brightness:
+        change_brightness_pct = (
+            current_brightness - new_brightness
+        ) / current_brightness
         ww_brightness = round(ww_brightness * (1 - change_brightness_pct))
         color_brightness = round(color_brightness * (1 - change_brightness_pct))
         cw_brightness = round(cw_brightness * (1 - change_brightness_pct))
     else:
-        change_brightness_pct = (brightness - current_brightness) / (
+        change_brightness_pct = (new_brightness - current_brightness) / (
             255 - current_brightness
         )
         ww_brightness = round(
@@ -201,14 +247,7 @@ def rgbww_brightness(
             (255 - cw_brightness) * change_brightness_pct + cw_brightness
         )
 
-    r, g, b = colorsys.hsv_to_rgb(h, s, color_brightness / 255)
-    return (
-        round(r * 255),
-        round(g * 255),
-        round(b * 255),
-        ww_brightness,
-        cw_brightness,
-    )
+    return color_brightness, cw_brightness, ww_brightness
 
 
 def color_temp_to_white_levels(temperature, brightness):
