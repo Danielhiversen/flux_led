7b9134704f2ee8e051fcc5a157abab92e60dc804
J. Nick Koston <nick@koston.org>
2021-11-14 11:21:27 -0600

    Refactor message reassembly to handle multiple messages in a single packet (#160)

--- flux_led/aiodevice.py
+++ flux_led/aiodevice.py
@@ -72,18 +72,19 @@ class AIOWifiLedBulb(LEDENETDevice):
 
     async def async_turn_on(self) -> None:
         """Turn on the device."""
-        await self._async_turn_on_with_retry()
-        self._set_power_state_ignore_next_push(self._protocol.on_byte)
+        if await self._async_turn_on_with_retry():
+            self._set_power_state_ignore_next_push(self._protocol.on_byte)
 
-    async def _async_turn_on_with_retry(self) -> None:
+    async def _async_turn_on_with_retry(self) -> bool:
         calls = (self._async_turn_on, self._async_turn_off_on, self._async_turn_on)
         for idx, call in enumerate(calls):
             if (
                 await self._async_execute_and_wait_for(self._on_futures, call)
                 or self.is_on
             ):
-                return
+                return True
             _LOGGER.debug("Failed to turn on (%s/%s)", 1 + idx, len(calls))
+        return False
 
     async def _async_turn_off(self) -> None:
         await self._async_send_msg(self._protocol.construct_state_change(False))
@@ -94,18 +95,19 @@ class AIOWifiLedBulb(LEDENETDevice):
 
     async def async_turn_off(self) -> None:
         """Turn off the device."""
-        await self._async_turn_off_with_retry()
-        self._set_power_state_ignore_next_push(self._protocol.off_byte)
+        if await self._async_turn_off_with_retry():
+            self._set_power_state_ignore_next_push(self._protocol.off_byte)
 
-    async def _async_turn_off_with_retry(self) -> None:
+    async def _async_turn_off_with_retry(self) -> bool:
         calls = (self._async_turn_off, self._async_turn_on_off, self._async_turn_off)
         for idx, call in enumerate(calls):
             if (
                 await self._async_execute_and_wait_for(self._off_futures, call)
                 or not self.is_on
             ):
-                return
+                return True
             _LOGGER.debug("Failed to turn off (%s/%s)", 1 + idx, len(calls))
+        return False
 
     async def async_set_white_temp(self, temperature, brightness, persist=True) -> None:
         """Set the white tempature."""
@@ -189,43 +191,24 @@ class AIOWifiLedBulb(LEDENETDevice):
         """New data on the socket."""
         start_empty_buffer = not self._buffer
         self._buffer += data
-        buffer = self._buffer
         self._updates_without_response = 0
-        msg_length = len(buffer)
-        protocol = self._protocol
-        # Some of the older bulbs respond to a state request in
-        # multiple packets so we have to reassemble.
-        if protocol.is_start_of_state_response(buffer):
-            if not protocol.is_valid_state_response(
-                buffer
-            ) and not protocol.is_longer_than_state_response(buffer):
+        msg_length = len(self._buffer)
+        while msg_length:
+            expected_length = self._protocol.expected_response_length(self._buffer)
+            if msg_length < expected_length:
+                # need more bytes
                 return
-            msg_length = protocol.state_response_length
-        if protocol.is_start_of_power_state_response(buffer):
-            if not protocol.is_valid_power_state_response(
-                buffer
-            ) and not protocol.is_longer_than_power_state_response(buffer):
-                return
-            msg_length = protocol.state_response_length
-        elif self.addressable:
-            # The addressable bulbs can send a state response inside an addressable response
-            if protocol.is_start_of_addressable_response(buffer):
-                if not protocol.is_valid_addressable_response(
-                    buffer
-                ) and not protocol.is_longer_than_addressable_response(buffer):
-                    return
-                msg_length = protocol.addressable_response_length
-
-        msg = buffer[:msg_length]
-        self._buffer = buffer[msg_length:]
-        if not start_empty_buffer:
-            _LOGGER.debug(
-                "%s <= Reassembled (%s) (%d)",
-                self._aio_protocol.peername,
-                " ".join(f"0x{x:02X}" for x in msg),
-                len(msg),
-            )
-        self._async_process_message(msg)
+            msg = self._buffer[:expected_length]
+            self._buffer = self._buffer[expected_length:]
+            msg_length = len(self._buffer)
+            if not start_empty_buffer:
+                _LOGGER.debug(
+                    "%s <= Reassembled (%s) (%d)",
+                    self._aio_protocol.peername,
+                    " ".join(f"0x{x:02X}" for x in msg),
+                    len(msg),
+                )
+            self._async_process_message(msg)
 
     def _async_process_message(self, msg):
         """Process a full message (maybe reassembled)."""
---
+++ tests_aio.py
@@ -0,0 +1,188 @@
+import asyncio
+from unittest.mock import MagicMock, patch
+import logging
+
+import pytest
+import contextlib
+
+from flux_led import aiodevice
+from flux_led.aio import AIOWifiLedBulb
+from flux_led.aioprotocol import AIOLEDENETProtocol
+from flux_led.const import COLOR_MODE_CCT, COLOR_MODE_RGBWW
+from flux_led.protocol import PROTOCOL_LEDENET_9BYTE
+
+
+@pytest.fixture
+async def mock_aio_protocol():
+    """Fixture to mock an asyncio connection."""
+    loop = asyncio.get_running_loop()
+    future = asyncio.Future()
+
+    async def _wait_for_connection():
+        await future
+        await asyncio.sleep(0)
+        await asyncio.sleep(0)
+
+    async def _mock_create_connection(func, ip, port):
+        protocol: AIOLEDENETProtocol = func()
+        transport = MagicMock()
+        protocol.connection_made(transport)
+        with contextlib.suppress(asyncio.InvalidStateError):
+            future.set_result(True)
+        return transport, protocol
+
+    with patch.object(loop, "create_connection", _mock_create_connection):
+        yield _wait_for_connection
+
+
+@pytest.mark.asyncio
+async def test_reassemble(mock_aio_protocol):
+    """Test we can reassemble."""
+    light = AIOWifiLedBulb("192.168.1.166")
+
+    def _updated_callback(*args, **kwargs):
+        pass
+
+    task = asyncio.create_task(light.async_setup(_updated_callback))
+    await mock_aio_protocol()
+    light._aio_protocol.data_received(
+        b"\x81\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xde"
+    )
+    await task
+    assert light.color_modes == {COLOR_MODE_RGBWW, COLOR_MODE_CCT}
+    assert light.protocol == PROTOCOL_LEDENET_9BYTE
+    assert light.model_num == 0x25
+    assert light.model == "WiFi RGBCW Controller (0x25)"
+    assert light.is_on is True
+    assert len(light.effect_list) == 20
+
+    light._aio_protocol.data_received(
+        b"\x81\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xde"
+        b"\x81\x25\x24\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xdf"
+    )
+    await asyncio.sleep(0)
+    assert light.is_on is False
+
+    light._aio_protocol.data_received(b"\x81")
+    light._aio_protocol.data_received(
+        b"\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f"
+    )
+    light._aio_protocol.data_received(b"\xde")
+    await asyncio.sleep(0)
+    assert light.is_on is True
+
+
+@pytest.mark.asyncio
+async def test_turn_on_off(mock_aio_protocol, caplog: pytest.LogCaptureFixture):
+    """Test we can turn on and off."""
+    light = AIOWifiLedBulb("192.168.1.166")
+
+    def _updated_callback(*args, **kwargs):
+        pass
+
+    task = asyncio.create_task(light.async_setup(_updated_callback))
+    await mock_aio_protocol()
+    light._aio_protocol.data_received(
+        b"\x81\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xde"
+    )
+    await task
+
+    task = asyncio.create_task(light.async_turn_off())
+    # Wait for the future to get added
+    await asyncio.sleep(0)
+    light._aio_protocol.data_received(
+        b"\x81\x25\x24\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xdf"
+    )
+    await asyncio.sleep(0)
+    assert light.is_on is False
+    await task
+
+    task = asyncio.create_task(light.async_turn_on())
+    await asyncio.sleep(0)
+    light._aio_protocol.data_received(
+        b"\x81\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xde"
+    )
+    await asyncio.sleep(0)
+    assert light.is_on is True
+    await task
+
+    await asyncio.sleep(0)
+    caplog.clear()
+    caplog.set_level(logging.DEBUG)
+    # Handle the failure case
+    with patch.object(aiodevice, "POWER_STATE_TIMEOUT", 0.05):
+        await asyncio.create_task(light.async_turn_off())
+        assert light.is_on is True
+        assert "Failed to turn off (1/3)" in caplog.text
+        assert "Failed to turn off (2/3)" in caplog.text
+        assert "Failed to turn off (3/3)" in caplog.text
+
+    with patch.object(aiodevice, "POWER_STATE_TIMEOUT", 0.05):
+        task = asyncio.create_task(light.async_turn_off())
+        # Do NOT wait for the future to get added, we know the retry logic works
+        light._aio_protocol.data_received(
+            b"\x81\x25\x24\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xdf"
+        )
+        await asyncio.sleep(0)
+        assert light.is_on is False
+        await task
+
+    await asyncio.sleep(0)
+    caplog.clear()
+    caplog.set_level(logging.DEBUG)
+    # Handle the failure case
+    with patch.object(aiodevice, "POWER_STATE_TIMEOUT", 0.05):
+        await asyncio.create_task(light.async_turn_on())
+        assert light.is_on is False
+        assert "Failed to turn on (1/3)" in caplog.text
+        assert "Failed to turn on (2/3)" in caplog.text
+        assert "Failed to turn on (3/3)" in caplog.text
+
+
+@pytest.mark.asyncio
+async def test_shutdown(mock_aio_protocol):
+    """Test we can shutdown."""
+    light = AIOWifiLedBulb("192.168.1.166")
+
+    def _updated_callback(*args, **kwargs):
+        pass
+
+    task = asyncio.create_task(light.async_setup(_updated_callback))
+    await mock_aio_protocol()
+    light._aio_protocol.data_received(
+        b"\x81\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xde"
+    )
+    await task
+
+    await light.async_stop()
+    await asyncio.sleep(0)  # make sure nothing throws
+
+
+@pytest.mark.asyncio
+async def test_handling_connection_lost(mock_aio_protocol):
+    """Test we can reconnect."""
+    light = AIOWifiLedBulb("192.168.1.166")
+
+    def _updated_callback(*args, **kwargs):
+        pass
+
+    task = asyncio.create_task(light.async_setup(_updated_callback))
+    await mock_aio_protocol()
+    light._aio_protocol.data_received(
+        b"\x81\x25\x23\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xde"
+    )
+    await task
+
+    light._aio_protocol.connection_lost(None)
+    await asyncio.sleep(0)  # make sure nothing throws
+
+    # Test we reconnect and can turn off
+    task = asyncio.create_task(light.async_turn_off())
+    # Wait for the future to get added
+    await asyncio.sleep(0.1)  # wait for reconnect
+    light._aio_protocol.data_received(
+        b"\x81\x25\x24\x61\x05\x10\xb6\x00\x98\x19\x04\x25\x0f\xdf"
+    )
+    await asyncio.sleep(0)
+    assert light.is_on is False
+    await task
