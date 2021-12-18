1093b786c9533c4f6e50f7eeb5b4396e5ea9a636
J. Nick Koston <nick@koston.org>
2021-12-17 21:21:16 -0600

    Rename model info to reduce string length

--- flux_led/models_db.py
+++ flux_led/models_db.py
@@ -36,24 +36,24 @@ from .protocol import (
 MODEL_INFO_NAMES = {
     "ZG-LX-FL": "Flood Light",  # Seen on 24w Flood light
     "ZG-BL": "",  # unknown
-    "ZG-BL-IR": "Infrared Remote",
-    "IR": "Infrared Remote",
+    "ZG-BL-IR": "IR",
+    "IR": "IR",
     "ZG-BL-EH7W": "Bulb 7w",
-    "ZG-BL-IH9WL": "Bulb 9w RF Remote",
-    "ZG-BL-IH9W": "Bulb 9w RF Remote",
+    "ZG-BL-IH9WL": "Bulb 9w RF",
+    "ZG-BL-IH9W": "Bulb 9w RF",
     "ZG-BL-CB1": "Ceiling Light",
     "ZG-LX-EJ9W": "Bulb 9w",
-    "RF": "RF Remote",
+    "RF": "RF",
     "LWS-BL": "Ceiling Light",
-    "LWS-LX-IR": "Ceiling Light IR Remote",
+    "LWS-LX-IR": "Ceiling Light IR",
     "ZG-BL-611HZ": "",  # unknown
     "ZG-BL-5V": "5v",
     "ZG-LX": "",  # Seen on floor lamp, v2 addressable, and Single channel controller
     "ZG-LX-UART": "",  # Seen on UK xmas lights 0x33, fairy controller, and lytworx
     "ZG-BL-PWM": "Flood Light",  # Seen on 40w Flood Light
     "ZG-ZW2": "",  # seen on 0x97 socket
-    "ZGIR44": "44 Key IR Remote",
-    "IR_ZG": "IR Remote",
+    "ZGIR44": "44 Key IR",
+    "IR_ZG": "IR",
 }
 
 
