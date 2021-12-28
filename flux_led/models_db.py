2690aed5642a4c4018f34afecb23bd0df9c7ea14
J. Nick Koston <nick@koston.org>
2021-12-28 13:09:44 -1000

    Fix CCT bulbs not being able to set temp (#282)

--- flux_led/models_db.py
+++ flux_led/models_db.py
@@ -882,9 +882,9 @@ MODELS = [
     ),
     LEDENETModel(
         model_num=0x52,
-        models=[],
+        models=["AK001-ZJ200"],
         description="Bulb CCT",
-        always_writes_white_and_colors=False,  # Formerly rgbwprotocol
+        always_writes_white_and_colors=True,  # Formerly rgbwprotocol
         protocols=[MinVersionProtocol(0, PROTOCOL_LEDENET_8BYTE)],
         mode_to_color_mode={},
         color_modes={COLOR_MODE_CCT},  # Formerly rgbwcapable
@@ -913,7 +913,7 @@ MODELS = [
         model_num=0x62,
         models=[],
         description="Controller CCT",
-        always_writes_white_and_colors=False,  # Formerly rgbwprotocol
+        always_writes_white_and_colors=True,  # Formerly rgbwprotocol
         protocols=[MinVersionProtocol(0, PROTOCOL_LEDENET_8BYTE)],
         mode_to_color_mode={},
         color_modes={COLOR_MODE_CCT},  # Formerly rgbwcapable
