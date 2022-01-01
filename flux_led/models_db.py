83951455af565ad78757230177759184f57ef78f
J. Nick Koston <nick@koston.org>
2021-12-31 16:24:26 -1000

    Add another firmware comment to 0x06

--- flux_led/models_db.py
+++ flux_led/models_db.py
@@ -588,7 +588,7 @@ MODELS = [
     LEDENETModel(
         model_num=0x06,
         # "AK001-ZJ2104" == v1 has RF remote support
-        # "AK001-ZJ2145" == v2.03 has IR remote support
+        # "AK001-ZJ2145" == v2.03, v2.09 has IR remote support
         # "AK001-ZJ2147" == v3.31 has 2.4ghz remote support
         models=[
             "AK001-ZJ100",
